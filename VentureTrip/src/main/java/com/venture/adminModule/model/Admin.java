c2a1add45f664d5b961874c108052266db2a7550
GOPAL KRUSHNA SAHOO <103574856+gopalkrushnas063@users.noreply.github.com>
2022-12-14 13:12:03 +0530

    Lombok getter setter method implmented in admin

--- VentureTrip/src/main/java/com/venture/adminModule/model/Admin.java
+++ VentureTrip/src/main/java/com/venture/adminModule/model/Admin.java
@@ -1,6 +1,24 @@
 package com.venture.adminModule.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer adminID;
     private String adminName;
     private String password;
